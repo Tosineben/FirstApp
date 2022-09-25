5cf653353126da07e87121f699a269c463b4320d
Tosineben <109301582+EbenezerIyaju@users.noreply.github.com>
2022-09-25 07:59:44 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by iyaju lekan  on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
